5cc2b993b2384b1fc08d9b30da1030b129395a1a
M-Orxan <orxanm385@gmail.com>
2024-11-11 17:25:01 +0400

    Stack<T> class implemented

---
+++ DataStructuresAndAlgorithms/DataStructures/Stack.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructuresAndAlgorithms.DataStructures
+{
+
+    public class Stack<T>
+    {
+        public List<T> StackList { get; set; }
+       
+
+        public Stack()
+        {
+            StackList = new List<T>();
+        }
+
+        /// <summary>
+        /// 	Push: Add an item to the top of the stack.
+        /// </summary>
+
+        public void Push(T item)
+        {
+            StackList.Add(item);
+        }
+
+
+        /// <summary>
+        /// 	Pop: Remove and return the item from the top of the stack.
+        /// </summary>
+
+        public T Pop()
+        {
+            if (StackList.Count > 0)
+            {
+                T lastItem = StackList[StackList.Count - 1];
+
+                StackList.Remove(lastItem);
+                return lastItem;
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty");
+                return default;
+            }
+
+        }
+
+
+        /// <summary>
+        /// 	Peek: Return the item at the top of the stack without removing it.
+        /// </summary>
+
+        public T Peek()
+        {
+            if (StackList.Count > 0)
+                return StackList[StackList.Count - 1];
+            else
+            {
+                Console.WriteLine("Stack is empty");
+                return default;
+
+            }
+        }
+
+
+
+        public void Print()
+        {
+            if (StackList.Count > 0)
+            {
+                foreach (T item in StackList)
+                {
+
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// 	Count: Gets the number of elements in the stack.
+        /// </summary>
+
+        public int Count()
+        {
+            return StackList.Count;
+        }
+
+
+        /// <summary>
+        /// 	Clear: Removes all items from the stack.
+        /// </summary>
+
+        public void Clear()
+        {
+            StackList = new List<T>();
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructuresAndAlgorithms.DataStructures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,6 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-
-
-
         }
     }
 }
