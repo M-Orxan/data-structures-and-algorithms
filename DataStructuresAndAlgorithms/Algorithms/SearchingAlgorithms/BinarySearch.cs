480e709b10272a3fd1ffbdd75d21684c7ddbac17
M-Orxan <orxanm385@gmail.com>
2024-11-13 23:14:39 +0400

    Binary search implemented

---
+++ DataStructuresAndAlgorithms/Algorithms/SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Algorithms.SearchingAlgorithms
+{
+    internal class BinarySearch<T> where T : IComparable<T>
+    {
+
+        public int Search(T targetValue, List<T> list)
+        {
+            int low = 0;
+            int high = list.Count - 1;
+
+
+            while (low <= high)
+            {
+
+                int middle = (low + high) / 2;
+
+                if (list[middle].Equals(targetValue))
+                {
+                    return middle;
+                }
+                else if (list[middle].CompareTo(targetValue) > 0)
+                {
+                    high = middle - 1;
+                }
+                else
+                {
+                    low = middle + 1;
+                }
+
+            }
+
+            return -1;
+
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,10 +1,12 @@
 ﻿using DataStructuresAndAlgorithms.Algorithms;
+using DataStructuresAndAlgorithms.Algorithms.SearchingAlgorithms;
 using DataStructuresAndAlgorithms.Algorithms.SortingAlgorithms;
 using DataStructuresAndAlgorithms.DataStructures;
 using DataStructuresAndAlgorithms.DataStructures.LinkedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +16,8 @@ namespace DataStructuresAndAlgorithms
     {
         static void Main(string[] args)
         {
-           
-          
-            
+
+
 
         }
     }
