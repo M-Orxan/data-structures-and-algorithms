50f2aab20ca57b639740dee86252eee877a54a35
M-Orxan <orxanm385@gmail.com>
2024-11-15 19:32:33 +0400

    Insertion sort implemented

---
+++ DataStructuresAndAlgorithms/Algorithms/SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms.Algorithms.SortingAlgorithms
+{
+    public class InsertionSort<T> where T : IComparable<T>
+    {
+        public void Sort(List<T> List)
+        {
+
+            for (int i = 1; i < List.Count; i++)
+            {
+
+                T temp = List[i];
+                int j = i - 1;
+
+                while (j >= 0 && temp.CompareTo(List[j]) < 0)
+                {
+                    List[j + 1] = List[j];
+                    j--;
+                }
+                List[j + 1] = temp;
+
+            }
+
+        }
+    }
+}
